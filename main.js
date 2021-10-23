a64ea9192d92cf2fe183bc9c1374516739311e6b
Eric Nguyen <11a1eric3@gmail.com>
2021-10-23 21:51:09 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({
+  static: "./images"
+});
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use('/api',router);
+// const PORT = process.env.PORT || 5000;
+server.listen(8000, () => {
+  console.log("JSON Server is running on port 8000");
+});
